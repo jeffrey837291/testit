c73a6f0fc1bda74b9de02fe88501ac80143987b3
jeffrey837291 <mars837291@gmail.com>
2021-09-03 14:03:14 +0800

    hello

---
+++ Untitled2.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include <iostream>
+ 
+int main()
+{
+  cout << "Hello world!" << endl;
+  cout << "Welcome to C++ programming!" << endl;
+  cout << "I am the second user" << endl;
+  return 0;
+}
